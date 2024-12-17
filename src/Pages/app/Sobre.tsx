e63f11a0ba6ef5125e8745c9d93ff34a883878de
GuilhermeBarbosa16 <guiba1616@gmail.com>
2024-12-17 09:40:40 -0300

    fix: Alteração de textos

--- src/Pages/app/Sobre.tsx
+++ src/Pages/app/Sobre.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Container, Box, Typography, Grid, Collapse } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { FaHtml5, FaCss3Alt, FaJs, FaLaravel, FaReact, FaNodeJs, FaPhp } from 'react-icons/fa';
+import { FaHtml5, FaCss3Alt, FaJs, FaLaravel, FaReact, FaNodeJs, FaGitAlt, FaPhp } from 'react-icons/fa';
 import { SiFigma, SiXampp } from 'react-icons/si';
 
 const Home: React.FC = () => {
@@ -18,9 +18,46 @@ const Home: React.FC = () => {
     { icon: <FaLaravel style={{ fontSize: 40, color: '#FF2D20' }} />, label: 'Laravel', percentage: 30 },
     { icon: <FaReact style={{ fontSize: 40, color: '#61DAFB' }} />, label: 'React', percentage: 45 },
     { icon: <FaNodeJs style={{ fontSize: 40, color: '#68A063' }} />, label: 'Node.js', percentage: 35 },
-    { icon: <FaPhp style={{ fontSize: 40, color: '#777BB4' }} />, label: 'PHP', percentage: 40 },  
+    { icon: <FaGitAlt style={{ fontSize: 40, color: '#F1502F' }} />, label: 'Git', percentage: 75 },
     { icon: <SiFigma style={{ fontSize: 40, color: '#F24E1E' }} />, label: 'Figma', percentage: 80 },
-    { icon: <SiXampp style={{ fontSize: 40, color: '#FB7A24' }} />, label: 'XAMPP', percentage: 100 },
+    { icon: <FaPhp style={{ fontSize: 40, color: '#777BB4' }} />, label: 'PHP', percentage: 75 },
+  ];
+
+  const experiences = [
+    {
+      panel: 'panel1',
+      title: 'Estagiário de help desk na Controladoria Geral do Estado',
+      date: 'Jul de 2023 - Abr de 2024',
+      description: (
+        <ul style={{ margin: 0, paddingLeft: '20px', textAlign: 'justify' }}>
+          <li>Suporte técnico básico em atividades de help desk, incluindo suporte para VPN e resolução de problemas cotidianos</li>
+        </ul>
+      ),
+    },
+    {
+      panel: 'panel2',
+      title: 'Estagiário de desenvolvimento na Controladoria Geral do Estado',
+      date: 'Abr de 2024 - Jul de 2024',
+      description: (
+        <ul style={{ margin: 0, paddingLeft: '20px', textAlign: 'justify' }}>
+          <li>Atuação como estagiário de desenvolvimento, contribuindo para a melhoria de sistemas internos.</li>
+          <li>Desenvolvimento com foco em frameworks PHP, utilizando Laravel e Joomla.</li>
+          <li>Participação no desenvolvimento de soluções para otimizar fluxos de trabalho e processos administrativos.</li>
+          <li>Fortalecimento de habilidades em desenvolvimento web e análise de requisitos técnicos.</li>
+        </ul>
+      ),
+    },
+    {
+      panel: 'panel3',
+      title: 'Análise e desenvolvimento de sistemas - Una',
+      date: 'Jan de 2022 - Dez de 2024',
+      description: (
+        <Typography variant="body2" sx={{ textAlign: 'justify', lineHeight: '1.6' }}>
+          5º Período. Ensino Superior em Análise de Sistemas, com competências como UML, MVC, Java, Oracle, MySQL, Node,
+          Express.js, React, Angular, PHP, TDD, BDD, Scrum e Kanban.
+        </Typography>
+      ),
+    },
   ];
 
   return (
@@ -46,30 +83,7 @@ const Home: React.FC = () => {
           Formação Acadêmica
         </Typography>
 
-        {/* Experiências */}
-        {[
-          {
-            panel: 'panel1',
-            title: 'Estagiário de help desk na Controladoria Geral do Estado',
-            date: 'Jul de 2023 - Abr de 2024',
-            description:
-              'Durante esse período fui responsável pelo suporte necessário à infraestrutura de rede e para os servidores públicos, assistência no desenvolvimento de sites e manutenção de sistemas utilizando Joomla.',
-          },
-          {
-            panel: 'panel2',
-            title: 'Estagiário de desenvolvimento na Controladoria Geral do Estado',
-            date: 'Abr de 2024 - Jul de 2024',
-            description:
-              'Trabalhei exclusivamente no desenvolvimento de sistemas, utilizando tecnologias como PHP, JavaScript, Laravel, Livewire e Joomla.',
-          },
-          {
-            panel: 'panel3',
-            title: 'Análise e desenvolvimento de sistemas - Una',
-            date: 'Jan de 2022 - Dez de 2024',
-            description:
-              '5º Período. Ensino Superior em Análise de Sistemas, com competências como UML, MVC, Java, Oracle, MySQL, Node, Express.js, React, Angular, PHP, TDD, BDD, Scrum e Kanban.',
-          },
-        ].map((experience) => (
+        {experiences.map((experience) => (
           <Box
             key={experience.panel}
             sx={{
@@ -103,9 +117,7 @@ const Home: React.FC = () => {
                 {experience.date}
               </Typography>
               <Collapse in={expanded === experience.panel} timeout="auto" unmountOnExit>
-                <Typography variant="body2" sx={{ marginTop: '10px', textAlign: 'justify', lineHeight: '1.6' }}>
-                  {experience.description}
-                </Typography>
+                {experience.description}
               </Collapse>
             </Box>
           </Box>
@@ -113,7 +125,7 @@ const Home: React.FC = () => {
       </Box>
 
       {/* Seção da direita: Habilidades */}
-      <Box sx={{ width: { xs: '100%', md: '38%' }, paddingLeft : '1.5rem' }}>
+      <Box sx={{ width: { xs: '100%', md: '38%' }, paddingLeft: '1.5rem' }}>
         <Typography variant="h4" sx={{ fontWeight: 'bold', textAlign: 'center', marginBottom: '20px' }}>
           Software's <br />
           <span style={{ color: '#03DAC6' }}>&</span>
@@ -128,11 +140,10 @@ const Home: React.FC = () => {
                   display: 'flex',
                   flexDirection: 'column',
                   alignItems: 'center',
-                  padding: '5px',      
+                  padding: '5px',
                   transition: 'transform 0.3s, border-color 0.3s',
                   '&:hover': {
                     transform: 'scale(1.05)',
-                    
                   },
                 }}
               >
