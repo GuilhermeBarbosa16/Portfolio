a84c51c5adea9f86e1764e7cf6af252286f7bdc5
GuilhermeBarbosa16 <guiba1616@gmail.com>
2025-03-10 10:26:28 -0300

    fix: debug do link artstar

--- src/Pages/app/Projects.tsx
+++ src/Pages/app/Projects.tsx
@@ -175,7 +175,7 @@ const Projects = () => {
               <GitHubIcon fontSize="large" />
             </Link>
             <Link
-              href="https://artstarmovelaria.com.br"
+              href="https://artsatarmovelaria.com.br/"
               target="_blank"
               rel="noopener"
               sx={{ color: "#fff", "&:hover": { color: "#00CFFF" } }}
